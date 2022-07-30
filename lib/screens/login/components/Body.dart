cfced82af9f64eab78215061c95eaa52f6b4bfe5
Ishtiaque Pial <ishtiaque.morshed@sslwireless.com>
2022-07-30 21:09:48 +0600

    code optimize

--- lib/screens/login/components/Body.dart
+++ lib/screens/login/components/Body.dart
@@ -10,47 +10,43 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Background(
+      child: SingleChildScrollView(
         child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        // bg
-        RoundedTextField(
-          hintText: "Phone Number",
-          iconUrl: "assets/icons/phone_call.svg",
-          keyboardType: TextInputType.number,
-          onChanged: (value){},
-        ) ,
-        SizedBox(height: 28),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // bg
+            RoundedTextField(
+              hintText: "Phone Number",
+              iconUrl: "assets/icons/phone_call.svg",
+              keyboardType: TextInputType.number,
+              onChanged: (value) {},
+            ),
+            SizedBox(height: 28),
 
-        RoundedTextField(
-          hintText: "Password",
-          iconUrl: "assets/icons/password.svg",
-          obscureText: true,
-          keyboardType: TextInputType.text,
-          onChanged: (value){},
+            RoundedTextField(
+              hintText: "Password",
+              iconUrl: "assets/icons/password.svg",
+              obscureText: true,
+              keyboardType: TextInputType.text,
+              onChanged: (value) {},
+            ),
+            SizedBox(height: 28),
+
+            Opacity(
+              // FORGOT PASSWORD
+              opacity: 0.6000000238418579,
+              child: Text("FORGOT PASSWORD",
+                  style: const TextStyle(
+                      color: const Color(0xffb4bbc9),
+                      fontWeight: FontWeight.w500,
+                      fontFamily: "Poppins",
+                      fontStyle: FontStyle.normal,
+                      fontSize: 12.0),
+                  textAlign: TextAlign.left),
+            )
+          ],
         ),
-
-        SizedBox(height: 28),
-
-        Opacity(  // FORGOT PASSWORD
-          opacity : 0.6000000238418579,
-          child:   Text(
-              "FORGOT PASSWORD",
-              style: const TextStyle(
-                  color:  const Color(0xffb4bbc9),
-                  fontWeight: FontWeight.w500,
-                  fontFamily: "Poppins",
-                  fontStyle:  FontStyle.normal,
-                  fontSize: 12.0
-              ),
-              textAlign: TextAlign.left
-          ),
-        )
-      ],
-    ));
+      ),
+    );
   }
 }
-
-
-
-
